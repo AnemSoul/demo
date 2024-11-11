3f953068c30ed4751687fee566ee5fd7bd9044b7
AnemSoul <119667666+AnemSoul@users.noreply.github.com>
2024-11-12 02:54:47 +0400

    new start for task05

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -1,76 +1,27 @@
 package com.task05;
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
-import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.task05.dto.Events;
-import com.task05.dto.Request;
-import com.task05.dto.Response;
 
-import java.time.Instant;
-import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @LambdaHandler(
-        lambdaName = "api_handler",
-        roleName = "api_handler-role",
-        isPublishVersion = true,
-        aliasName = "${lambdas_alias_name}",
-        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+    lambdaName = "api_handler",
+	roleName = "api_handler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@EnvironmentVariables(value = {
-        @EnvironmentVariable(key = "target_table", value = "$(target_table)")
-})
-public class ApiHandler implements RequestHandler<Request, Response> {
+public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
 
-    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
-    private final DynamoDB dynamoDb = new DynamoDB(client);
-    private final String DYNAMODB_TABLE_NAME = System.getenv("target_table");
-
-    public Response handleRequest(Request request, Context context) {
-
-        int principalID = request.getPrincipalID();
-        Map<String, String> body = request.getContent();
-
-        String newId = UUID.randomUUID().toString();
-        String createdAt = DateTimeFormatter
-                .ISO_INSTANT
-                .format(Instant.now().atOffset(ZoneOffset.UTC));
-
-        Table table = dynamoDb.getTable(DYNAMODB_TABLE_NAME);
-
-        Item item = new Item()
-                .withPrimaryKey("id", newId)
-                .withInt("principalID", principalID)
-                .withString("createdAt", createdAt)
-                .withMap("body", body);
-
-        table.putItem(item);
-
-        Events events = Events
-                .builder()
-                .id(newId)
-                .principalID(principalID)
-                .createdAt(createdAt)
-                .body(body)
-                .build();
-
-        Response response = Response
-                .builder()
-                .statusCode(201)
-                .events(events)
-                .build();
-
-        return response;
-    }
+	public Map<String, Object> handleRequest(Object request, Context context) {
+		System.out.println("Hello from lambda");
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", 200);
+		resultMap.put("body", "Hello from Lambda");
+		return resultMap;
+	}
 }
