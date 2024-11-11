1762e65c2f389f099f67bace25cad5f2045f8cfd
Alekseii Guskov <ingrzhd@gmail.com>
2024-11-12 01:27:04 +0400

    new fix 02

--- task05/jsrc/main/java/com/task05/dto/Events.java
+++ task05/jsrc/main/java/com/task05/dto/Events.java
@@ -1,42 +1,17 @@
 package com.task05.dto;
 
+import lombok.*;
+
 import java.util.Map;
 
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Events {
     private String id;
     private int principalID;
     private String createdAt;
     private Map<String, String> body;
-
-    public Map<String, String> getBody() {
-        return body;
-    }
-
-    public void setBody(Map<String, String> body) {
-        this.body = body;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getPrincipalID() {
-        return principalID;
-    }
-
-    public void setPrincipalID(int principalID) {
-        this.principalID = principalID;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
 }
