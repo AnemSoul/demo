41e87a99d380fe1ee3e06fd0dcf7bdcbbf36a927
Alekseii Guskov <ingrzhd@gmail.com>
2024-11-12 01:27:04 +0400

    new fix 02

--- task05/jsrc/main/java/com/task05/dto/Response.java
+++ task05/jsrc/main/java/com/task05/dto/Response.java
@@ -1,29 +1,13 @@
 package com.task05.dto;
 
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Response {
     private int statusCode;
     private Object events;
-
-    public Response() {}
-
-    public Response(int statusCode, Object event){
-        this.statusCode = statusCode;
-        this.events = event;
-    }
-
-    public int getStatusCode(int i) {
-        return statusCode;
-    }
-
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
-    public Object getEvents() {
-        return events;
-    }
-
-    public void setEvents(Object events) {
-        this.events = events;
-    }
 }
