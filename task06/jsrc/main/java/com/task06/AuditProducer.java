b1dffab82f7f652d0c05ed041c06dd7ef4b8863d
AnemSoul <119667666+AnemSoul@users.noreply.github.com>
2024-11-12 16:52:33 +0400

    Task06 step 5 in the progress

--- task06/jsrc/main/java/com/task06/AuditProducer.java
+++ task06/jsrc/main/java/com/task06/AuditProducer.java
@@ -2,6 +2,9 @@ package com.task06;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
+import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.model.RetentionSetting;
 
@@ -14,6 +17,13 @@ import java.util.Map;
 	isPublishVersion = true,
 	aliasName = "${lambdas_alias_name}",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@EnvironmentVariables(
+		@EnvironmentVariable(key = "target_table", value = "$(target_table)")
+)
+@DynamoDbTriggerEventSource(
+		targetTable = "Configuration",
+		batchSize = 1
 )
 public class AuditProducer implements RequestHandler<Object, Map<String, Object>> {
 
