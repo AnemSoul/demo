140aee30a6351a1936bbef976066c2ee66c6259c
AnemSoul <119667666+AnemSoul@users.noreply.github.com>
2024-11-14 20:57:40 +0400

    task08 5d folder 1nd try

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,42 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import org.example.OpenMeteoWeather;
+
+@LambdaHandler(
+        lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        layers = {"sdk-layer"},
+        isPublishVersion = true,
+        aliasName = "${lambdas_alias_name}",
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+)
+@LambdaLayer(
+        layerName = "sdk-layer",
+        libraries = {"layer/lib/open-weather-1.0-SNAPSHOT.jar"},
+        runtime = DeploymentRuntime.JAVA11,
+        artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
+)
+public class ApiHandler implements RequestHandler<Object, String> {
+
+    public String handleRequest(Object request, Context context) {
+
+        OpenMeteoWeather openMeteoWeather = new OpenMeteoWeather();
+        String response = openMeteoWeather.callApi();
+        context.getLogger().log(response);
+        return openMeteoWeather.callApi();
+    }
+}
